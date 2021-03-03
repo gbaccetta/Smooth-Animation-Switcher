71ad1389480150bc97e620750dcecb47f3b2fb5c
Giovanni Accetta <giovanni.accetta@orange.com>
2021-03-03 16:27:21 -0400

    small fix

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,5 +1,4 @@
 export 'demo_view_animated_switcher.dart';
 export 'demo_view_no_animation.dart';
 export 'demo_view_smooth_animated_switcher.dart';
-export '
-animated_size_widget.dart';
+export 'animated_size_widget.dart';
